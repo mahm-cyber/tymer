01e51453e320d5fb47110e236833032dc95db473
Fareed <mohamedfareedm@hotmail.com>
2024-12-29 19:47:15 +0200

    a commit

--- packages/user_repository/lib/src/user_repository.dart
+++ packages/user_repository/lib/src/user_repository.dart
@@ -533,7 +533,7 @@ class UserRepository {
         }
       }
 
-      // await Future.delayed(const Duration(milliseconds: 10));
+      await Future.delayed(const Duration(milliseconds: 10));
       final locationData = await location.getLocation();
       return locationData;
     } catch (error) {
